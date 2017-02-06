fb16363bd06275f25f4d63a4d2d7303a58b3cdc8
Nicholas Hall <NickJohnHall@gmail.com>
2017-02-05 18:24:44 -0600

    some work with jesse

--- main.py
+++ main.py
@@ -14,12 +14,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render("new_post_views.html")
+
+class NewPost(Handler):
+    def get(self):
+        self.render("new_post.html")
+
+    def post(self):
+        title = self.request.get("title")
+        blogcontent = self.request.get("blogcontent")
+        error_title = ""
+        error_content = ""
+
+
+        if title and blogcontent:
+            self.write("hey")
+        else:
+            if title:
+                error_content = "We need Content"
+            if blogcontent:
+                error_title = "We need title"
+            self.render("new_post.html",title = title, blogcontent = blogcontent, error_content = error_content, error_title = error_title)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/blog', MainHandler),
+    ('/newpost', NewPost),
 ], debug=True)
